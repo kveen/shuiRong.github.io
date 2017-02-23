756385d6df6212c72568a229116b243df704b9c5
shuirong <shuirong1997@icloud.com>
2017-02-23 12:12:18 +0800

    Site updated: 2017-02-23 12:12:18

--- xl/files/javascript/main.js
+++ xl/files/javascript/main.js
@@ -7,6 +7,9 @@ function loadStuff(){
     //根据屏幕高度设置mainSec.动态大小屏
     var bgHeight = window.screen.height * 0.75 + 'px';
     $('#mainSec').css('height',bgHeight);
+    //移动端,根据屏幕高度设置div高 
+    var mobileHeight = document.documentElement.clientHeight + 'px';
+    $('#mobile').css('height',mobileHeight);
 }
 
 function listen(){
@@ -15,6 +18,5 @@ function listen(){
     });
     $('#cancelBtn').click(function(){
         $('#downloadSec').css('visibility','hidden');
-    });
-    //移动端,根据屏幕高度设置div高
+    });    
 }
