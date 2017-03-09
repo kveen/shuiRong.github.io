52dcf23f2acf0c9029bc3e504c13626d60b8706c
shuirong <shuirong1997@icloud.com>
2017-03-09 22:56:52 +0800

    Site updated: 2017-03-09 22:56:51

--- js/start.js
+++ js/start.js
@@ -3,11 +3,10 @@
     hljs.initHighlightingOnLoad();
     //根据屏幕
     var width = document.documentElement.clientWidth;
-    alert(width);
     if(width < 500){
         $('#postContainer').css({
             'width': width * 0.9 + 'px',
-            'margin': '0 auto'
+            'margin': '10px auto'
         })
     }
     
