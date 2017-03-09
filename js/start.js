a0e7b955b50534b30bae7715d1df8ca9dac114df
shuirong <shuirong1997@icloud.com>
2017-03-09 22:55:34 +0800

    Site updated: 2017-03-09 22:55:34

--- js/start.js
+++ js/start.js
@@ -2,5 +2,13 @@
     //运行highlight.js
     hljs.initHighlightingOnLoad();
     //根据屏幕
-
+    var width = document.documentElement.clientWidth;
+    alert(width);
+    if(width < 500){
+        $('#postContainer').css({
+            'width': width * 0.9 + 'px',
+            'margin': '0 auto'
+        })
+    }
+    
 })();
